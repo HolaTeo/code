43f3d8464f7e380ec9287388f612b3ca566a7fed
HolaTeo <holateo22@gmail.com>
2020-07-01 23:44:06 +0900

    day18

---
+++ 01_Python/school/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dss",
+    packages = find_packages(),
+    include_package_data=True,
+    version = "1.0.0",
+    author = "holateo",
+    author_email = "holateo22@gmail.com",
+    zip_safe=False,
+)
